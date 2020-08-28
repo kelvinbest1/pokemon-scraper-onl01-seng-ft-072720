ed3ff50d2a936e70c45b6e9ba66a13fe1838c2a5
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-28 13:57:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,10 +8,7 @@ class Scraper
          @type = type
          @hp = hp
          @db = db
-    self.db = db
-    self.file = File.open("pokemon_index.html")
-    self.parsed_file = Nokogiri::HTML.parse(file)
-    self.all_pokemon = catch_em_all
+    
   end
 
   def catch_em_all
